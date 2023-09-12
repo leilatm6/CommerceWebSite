b09e2d37eae97fec335031013250aec83b28994e
leila <leilatm6@hotmail.com>
2023-09-12 09:26:08 +0400

    NewList Done

--- auctions/models.py
+++ auctions/models.py
@@ -22,10 +22,13 @@ class AuctionListing(models.Model):
     imageurl = models.URLField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name='sellingproduct')
-    category = models.ForeignKey(
-        Category, on_delete=models.DO_NOTHING, blank=True)
+    """category = models.ForeignKey(
+        Category, on_delete=models.DO_NOTHING, blank=True)"""
     watchedby = models.ManyToManyField(
         User, related_name='watchlist', blank='True')
+
+    def __str__(self):
+        return f"{self.title}: {self.description} with price {self.startingbid} and user {self.user}"
 
 
 class Bids(models.Model):
--- auctions/views.py
+++ auctions/views.py
@@ -3,17 +3,17 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
-from .models import User
+from .models import User, Category, AuctionListing
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    return render(request, "auctions/index.html", {
+        "lists": AuctionListing.objects.all()
+    })
 
 
 def login_view(request):
     if request.method == "POST":
-
         # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
@@ -57,8 +57,32 @@ def register(request):
             return render(request, "auctions/register.html", {
                 "message": "Username already taken."
             })
-        print('salam')
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def newlist(request):
+    if request.method == "POST":
+        if not request.user.is_authenticated:
+            return render(request, "auctions/login.html")
+        if request.POST["title"] == '' or request.POST["description"] == '' or request.POST["intialbid"] == '':
+            return render(request, "auctions/newlist.html", {
+                #  'categories': Category,
+                'message': "Please Fill all mandatory Fields"
+            })
+
+        title = request.POST["title"]
+        description = request.POST["description"]
+        initial_bid = request.POST["intialbid"]
+        photo = request.POST["photo"]
+        print(title, description, initial_bid, photo)
+        newlist = AuctionListing(
+            title=title, description=description, startingbid=initial_bid, imageurl=photo, user=request.user)
+        print(newlist)
+        newlist.save()
+        return HttpResponseRedirect(reverse('index'))
+    return render(request, "auctions/newlist.html", {
+        # 'categories': Category
+    })
